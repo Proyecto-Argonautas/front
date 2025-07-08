30f4abba73e4d2e30b9061d4da63a002412b1303
Autovot <87210193+Autovot@users.noreply.github.com>
2025-07-08 13:18:36 +0200

    Route first component

---
+++ app/pages/landing.tsx
@@ -0,0 +1,9 @@
+import MainMenuBar from "../components/MainMenuBar";
+
+export function Landing() {
+  return (
+    <main className="flex items-center justify-center pt-16 pb-4">
+      <MainMenuBar/>
+    </main>
+  );
+}
