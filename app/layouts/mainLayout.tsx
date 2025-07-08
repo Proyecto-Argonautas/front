517f91f83480ab11453479640eeccece2ffb82b8
Albert Gonzalez <albgonzalez98@gmail.com>
2025-07-08 13:06:13 +0200

    primeros-pasos-albert

---
+++ app/layouts/mainLayout.tsx
@@ -0,0 +1,11 @@
+
+
+function App({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="relative min-h-screen pb-16">
+      {children}
+    </div>
+  );
+}
+
+export default App; 
