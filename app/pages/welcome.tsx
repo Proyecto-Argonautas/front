7753090c6ee855f3215c163081d742b5e8cc99ff
Autovot <87210193+Autovot@users.noreply.github.com>
2025-07-10 00:06:58 +0200

    First route with button, second part

--- app/pages/welcome.tsx
+++ app/pages/welcome.tsx
@@ -1,16 +1,17 @@
 import { Link } from "react-router";
 
 export function Welcome() {
-	return (
-		<main className="flex flex-col items-center justify-center pt-16 pb-4">
-			<p>Hello World</p>
-			<Link to="/test">
-				<button
-					type="button"
-					className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-indigo-600" >
-					Test
-				</button>
-			</Link>
-		</main>
-	);
+  return (
+    <main className="flex flex-col items-center justify-center pt-16 pb-4">
+      <p>Hello World</p>
+      <Link to="/test">
+        <button
+          type="button"
+          className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Test
+        </button>
+      </Link>
+    </main>
+  );
 }
