96159eef4106492c4c9818bc59faca038cde843f
Autovot <87210193+Autovot@users.noreply.github.com>
2025-07-10 19:15:30 +0200

    Prepare pull request

--- app/pages/welcome.tsx
+++ app/pages/welcome.tsx
@@ -6,8 +6,8 @@ export function Welcome() {
       <p>Hello World</p>
       <Link to="/test">
         <button
-          type="button"
           className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          type="button"
         >
           Test
         </button>
--- app/routes.ts
+++ app/routes.ts
@@ -1,16 +1,9 @@
-import { index, prefix, type RouteConfig, route } from "@react-router/dev/routes";
+import {
+  index,
+  type RouteConfig,
+} from "@react-router/dev/routes";
 
 export default [
   // "/" Ruta raíz
-  index( "routes/home.tsx" ),
-
-  // "/test" Ruta test
-  route( "test", "routes/test.tsx" ),
-
-  // "/anidado" si no se le pone index, esta ruta no ofrece nada
-  ...prefix("anidado", [
-
-    // "/anidado/test" al usar prefix ya estamos dentro de una "carpeta"
-    route( "test", "routes/test2.tsx" )
-  ])
+  index("routes/home.tsx")
 ] satisfies RouteConfig;
