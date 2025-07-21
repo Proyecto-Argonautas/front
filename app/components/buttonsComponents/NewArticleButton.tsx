881f76e00a3bafdccce8f6ad24e18b67f9fe0a20
Albert Gonzalez <albgonzalez98@gmail.com>
2025-07-21 19:01:16 +0200

    fixed error

--- app/components/buttonsComponents/NewArticleButton.tsx
+++ app/components/buttonsComponents/NewArticleButton.tsx
@@ -1,4 +1,4 @@
-// ctypoomponents/NewArticleButton.tsx
+
 import { BedDouble, NotebookPen, PlaneTakeoff } from "lucide-react";
 import { useState } from "react";
 import FlightArticle from "../mainPage/FlightsArticle";
