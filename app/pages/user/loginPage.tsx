4f9b90ca4e5d5c9b04fdc3d6dadb92f68c378f74
Albert Gonzalez <albgonzalez98@gmail.com>
2025-07-29 13:22:19 +0200

    pages folders ordered p2

--- app/pages/user/loginPage.tsx
+++ app/pages/user/loginPage.tsx
@@ -1,7 +1,7 @@
 import MenuBar from "~/components/bars/MenuBar";
 import HomeButton from "~/components/buttonsComponents/HomeButton";
 import ReturnButton from "~/components/buttonsComponents/ReturnButton";
-import LoginCard from "../components/login/LoginCard";
+import LoginCard from "~/components/login/LoginCard";
 
 export function meta() {
   return [{ title: "Login" }, { name: "resume", content: "Login" }];
