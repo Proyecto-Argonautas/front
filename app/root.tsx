2540849b248de55e1a52c89a847bc12f724f3a5a
Autovot <87210193+Autovot@users.noreply.github.com>
2025-08-06 12:53:12 +0200

    Installed react router plugin vercel

--- app/root.tsx
+++ app/root.tsx
@@ -23,7 +23,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta charSet="utf-8" />
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <Meta />
-        {/*<Links />*/}
+        <Links />
       </head>
       <body>
         <UserProvider>{children}</UserProvider>
