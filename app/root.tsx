5c05dccff87d67f3cdb9c55c4f7f5e498f5ecc1e
autogit@aurena.ovh <87210193+Autovot@users.noreply.github.com>
2025-08-07 15:11:19 +0000

    Hydration fallback

--- app/root.tsx
+++ app/root.tsx
@@ -50,6 +50,10 @@ export default function App() {
   return <Outlet />;
 }
 
+export function HydrateFallback() {
+  return <p>Loading App...</p>;
+}
+
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
   let message = "Oops!";
   let details = "An unexpected error occurred.";
